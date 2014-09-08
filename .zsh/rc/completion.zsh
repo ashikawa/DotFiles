e8589fed52161defacc3790973298d9f5aa442fa
s.ashikawa <a.shigeru@gmail.com>
2014-09-08 16:50:30 +0900

    Update completion.zsh

--- .zsh/rc/completion.zsh
+++ .zsh/rc/completion.zsh
@@ -1,8 +1,9 @@
-autoload -U compinit
-compinit -u
-
 fpath=(/usr/local/share/zsh-completions $fpath)
 fpath=(/usr/local/Cellar/zsh-completions/0.11.0/share/zsh-completions $fpath)
+fpath=(~/.zsh/plugins $fpath)
+
+autoload -U compinit
+compinit -u
 
 hosts=( ${(@)${${(M)${(s:# :)${(zj:# :)${(Lf)"$([[ -f ~/.ssh/config ]] && < ~/.ssh/config)"}%%\#*}}##host(|name) *}#host(|name) }/\*} )
 zstyle ':completion:*:hosts' hosts $hosts
